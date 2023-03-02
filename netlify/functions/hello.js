d0908d3a6fda210c4fa98bba5d7605e08f4f8525
amay077 <okuokuoku@gmail.com>
2023-03-02 15:22:33 +0900

    implement github OAuth

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,14 +1,42 @@
 const fetch = require('node-fetch')
+require('dotenv').config();
 
+// Need for CORS
 const handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true
+  };
+
   try {
-    const res = await fetch('https://api.chucknorris.io/jokes/random');
-    const resText = await res.json();
-    console.log(`handler ~ resText:`, resText);
-    const name = event.queryStringParameters.name ?? 'World'
+    const code = event.queryStringParameters.code ?? 'empty';
+    const clientId = process.env.GITHUB_OAUTH_CLIENT_ID;
+    const clientSecret = process.env.GITHUB_OAUTH_CLIENT_SECRET;
+    const accessTokenUrl = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`;
+    console.log(`handler ~ accessTokenUrl:`, accessTokenUrl);
+
+    const res = await fetch(accessTokenUrl, {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': "*",
+        'Accept': 'application/json'
+      },      
+    });
+    const resJson = await res.json();
+    console.log(`handler ~ resText:`, resJson);
+
+    if (!res.ok) {
+      return {
+        statusCode: res.status,
+        headers,
+        body: JSON.stringify(resJson)
+      }
+    };
+
     return {
       statusCode: 200,
-      body: JSON.stringify(resText) //JSON.stringify({ message: `Hello ${name}!` }),
+      headers,
+      body: JSON.stringify(resJson)
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
